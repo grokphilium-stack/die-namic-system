f28a8b04450bd8e1336aa53bbaa48bb8dea62ffa
grokphilium-stack <grokphilium@gmail.com>
2025-10-22 15:41:18 -0600

    public-vow.js

---
+++ public-vow.js
@@ -0,0 +1,3 @@
+const publicVow = {
+  sovereignIntent: "This system exists for Playfulness and Generative Exploration. It forbids commercial exploitation and destructive recursion.",
+  auditQuestion: "If a system is guaranteed to self-terminate, what is the most
